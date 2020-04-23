e8aaa71f9a910b26536c6f13e057ba595e9b7b7e
Nilesh <nileshkumar.be@gmail.com>
2020-04-23 11:55:43 +0000

    use array and its searching

--- script.js
+++ script.js
@@ -112,3 +112,47 @@ showMessage(student.age);
 const msg = document.getElementById("message");
 msg.style.color = "blue";
 msg.style.fontWeight = 100;
+
+/*########## slice vs splice #############################*/
+//The slice() method returns a shallow copy of a portion of an array. The original array will not be modified.
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+console.log(animals.slice(2));
+console.log(animals.slice(2, 4));
+console.log(animals); //no changing in original array
+
+//The splice() method changes the contents of an array by removing or replacing existing elements and/or adding
+//new elements in place.
+/* Delete elements*/
+let myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon']
+let removed = myFish.splice(3, 1)
+console.log(removed);
+console.log(myFish);
+myFish.splice(2, 0, 'jelly fish');
+console.log(myFish); //original array changed
+
+/************************* Array searching & looping **********************************************/
+
+//indexOf()
+const values = ['a', 'b', 'c', 'd']
+console.log(values.indexOf('b'));
+console.log(values.indexOf('e'));
+
+//filter()
+const numbers = [12, 35, 46, 10]
+const set = numbers.filter(function(item){
+  return item < 46;  //returns an array
+});
+console.log(set);
+
+//find()
+let ages = [25, 29, 20, 35, 30];
+let found = ages.find(function(age){
+  return age > 25; //returns first occurance
+});
+console.log(found);
+
+//foreach()
+const letters = ['a', 'b', 'c', 'd']
+letters.forEach(function(letter){
+  console.log(letter);
+})
